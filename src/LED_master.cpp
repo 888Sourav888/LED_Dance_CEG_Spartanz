8855ff7daeb8d69a3bc5a65f6897d87bd9c91d7c
888Sourav888 <77108272+888Sourav888@users.noreply.github.com>
2024-02-20 00:14:45 +0530

    added the web server code for the master

---
+++ src/LED_master.cpp
@@ -0,0 +1,125 @@
+// //this file is responsible for having the code for the lead dancer
+// // the esp32 of this dancer will act as both master in the ESP-Now protocol
+// // as well as a web server that will be getting HTTP requests from the client to turn on or off
+
+// #include <esp_now.h>
+// #include <WiFi.h>
+// #include "ESPAsyncWebServer.h"
+// #include <Arduino_JSON.h>
+
+
+// // Replace with your network credentials (WIFI-STATION)
+// const char* ssid = "REPLACE_WITH_YOUR_SSID";
+// const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+
+// typedef struct struct_message {
+//   int id;
+//   int toggle_on ; 
+// } struct_message;
+
+
+// struct_message incomingCircuitOnMessage ; 
+
+// JSONVar board;
+
+// AsyncWebServer server(80);
+// AsyncEventSource events("/events");
+
+
+// //This callback function will be triggered when the webpage client makes request to the webserver 
+// void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) { 
+//   // Copies the sender mac address to a string
+//   char macStr[18];
+//   Serial.print("Packet received from: ");
+//   snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+//            mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+//   Serial.println(macStr);
+//   memcpy(&incomingCircuitOnMessage, incomingData, sizeof(incomingCircuitOnMessage));
+  
+
+
+//     board["id"] = incomingCircuitOnMessage.id ; 
+//     board["turnOn"] = incomingCircuitOnMessage.toggle_on ; 
+//   String jsonString = JSON.stringify(board);
+//   events.send(jsonString.c_str(), "new_readings", millis());
+  
+//   Serial.printf("Board ID %u: %u bytes\n", incomingCircuitOnMessage.id, len);
+//   Serial.printf("Spartanz member wants the circuit to be on ? (0/1):" , incomingCircuitOnMessage.toggle_on) ; 
+//   Serial.println();
+// }
+
+
+// const char index_html[] PROGMEM = R"rawliteral(
+// <!DOCTYPE HTML><html>
+// <head>
+//    html code for the webpage must be added here 
+//   </style>
+// </head>
+// <body>
+ 
+// </body>
+// </html>)rawliteral";
+
+
+
+// void setup(){
+
+//     // Initialize Serial Monitor
+//   Serial.begin(115200);
+
+//   // Set the device as a Station and Soft Access Point simultaneously
+//   WiFi.mode(WIFI_AP_STA);
+  
+//   // Set device as a Wi-Fi Station
+//   WiFi.begin(ssid, password);
+//   while (WiFi.status() != WL_CONNECTED) {
+//     delay(1000);
+//     Serial.println("Setting as a Wi-Fi Station..");
+//   }
+//   Serial.print("Station IP Address: ");
+//   Serial.println(WiFi.localIP());
+//   Serial.print("Wi-Fi Channel: ");
+//   Serial.println(WiFi.channel());
+//   Serial.print("ESP Board MAC Address: "); 
+//   Serial.println(WiFi.macAddress()) ; //printing the mac address if needed 
+
+ 
+//   // Init ESP-NOW
+//   if (esp_now_init() != ESP_OK) {
+//     Serial.println("Error initializing ESP-NOW");
+//     return;
+//   }
+  
+//   // Once ESPNow is successfully Init, we will register for recv CB to
+//   // get recv packer info
+//   esp_now_register_recv_cb(OnDataRecv);
+
+//   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+//     request->send_P(200, "text/html", index_html);
+//   });
+   
+//   events.onConnect([](AsyncEventSourceClient *client){
+//     if(client->lastId()){
+//       Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+//     }
+//     // send event with message "hello!", id current millis
+//     // and set reconnect delay to 1 second
+//     client->send("hello!", NULL, millis(), 10000);
+//   });
+//   server.addHandler(&events);
+//   server.begin();
+
+// }
+
+// void loop(){
+//   static unsigned long lastEventTime = millis();
+//   static const unsigned long EVENT_INTERVAL_MS = 5000;
+//   if ((millis() - lastEventTime) > EVENT_INTERVAL_MS) {
+//     events.send("ping",NULL,millis());
+//     lastEventTime = millis();
+//   }
+// }
+
+
+
